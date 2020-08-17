c7439923e572dd7dc2fb9064f8613b9b77d07edf
LuckyMasharane <lmasharane@gmail.com>
2020-08-18 01:07:05 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -7,19 +7,19 @@ import './style.css';
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
-let toDo_list: Array<any> = [{TaskName:"GRAPHQL",TaskDate:"12/july/2020",TaskStatus:"done"},
-{TaskName:"HTML5",TaskDate:"18/july/2020",TaskStatus:"done"},
-{TaskName:"CSS3",TaskDate:"22/july/2020",TaskStatus:"pending"},
-{TaskName:"JAVASCRIPT",TaskDate:"28/july/2020",TaskStatus:"done"},
-{TaskName:"TypeScript",TaskDate:"17/August/2020",TaskStatus:"done"},
-{TaskName:"C++",TaskDate:"12/May/2020",TaskStatus:"done"},
-{TaskName:"C#",TaskDate:"12/july/2019",TaskStatus:"done"},
-{TaskName:"REACTJS",TaskDate:"12/February/2020",TaskStatus:"done"},
-{TaskName:"ANGULERJS",TaskDate:"12/March/2020",TaskStatus:"pending"},
-{TaskName:"IONIC",TaskDate:"12/july/2020",TaskStatus:"done"}]
+let toDo_list: Array<any> = [{TaskName:"GRAPHQL",TaskDate:"12/july/2020",TaskStatus:"Done"},
+{TaskName:"HTML5",TaskDate:"18/july/2020",TaskStatus:"Done"},
+{TaskName:"CSS3",TaskDate:"22/july/2020",TaskStatus:"Pending"},
+{TaskName:"JAVASCRIPT",TaskDate:"28/july/2020",TaskStatus:"Done"},
+{TaskName:"TypeScript",TaskDate:"17/August/2020",TaskStatus:"Done"},
+{TaskName:"C++",TaskDate:"12/May/2020",TaskStatus:"Done"},
+{TaskName:"C#",TaskDate:"12/july/2019",TaskStatus:"Done"},
+{TaskName:"REACTJS",TaskDate:"12/February/2020",TaskStatus:"Done"},
+{TaskName:"ANGULERJS",TaskDate:"12/March/2020",TaskStatus:"Pending"},
+{TaskName:"IONIC",TaskDate:"12/july/2020",TaskStatus:"Done"}]
 
 for(var i= 0; i< toDo_list.length;i++){
-  if(toDo_list[4].TaskStatus ==="done"){
+  if(toDo_list[4].TaskStatus ==="Done"){
     toDo_list[4].TaskStatus = "Pending";
   }
   else{
